5586d0c0d0af175bcebc48031ac90d384990a714
u23549743 <u23549743@tuks.co.za>
2026-09-04 13:18:25 +0200

    04/09

---
+++ stock_model.py
@@ -0,0 +1,55 @@
+#days_late=3
+#days_late=days_late+1
+#if days_late > 3 :
+    # : is for indentation 
+    #print("Shipment is late")
+#else:
+ #   print("try")
+#livhuwani = 21
+#ndou = 17
+
+#if livhuwani > ndou:
+ #   print("Livhuwani is older than Ndou")
+#else:
+ #   print("Ndou is older than Livhuwani")
+
+#if livhuwani > 100 and livhuwani < 200:
+    #print("Livhuwani is between 100 and 200")
+#elif livhuwani < 200:
+  #  print("Livhuwani is less than 200")
+
+#for i in range(5):
+ #   print (i) 
+    #dictionary
+    #"name"
+#student = {"name": livhuwani, "age": 21, "gender": "male"}
+#print(student.get('age'))
+
+#ITERATION 
+#days_left = 5
+#while days_left > 0:
+ #   print("Your order will arrive in", days_left)
+  #  days_left -= 1
+#print("Your order has arrived")
+#courses = ["BFB321", "BSS300", "BDF200", "BSS200"]
+#for course in courses:
+ #   print(course)
+  #  if course == "BDF200":
+   #     print("This is the course you are looking for")
+    #else:
+     #   print(course)
+
+#function
+def check_grade(score):
+    if score >= 80:
+      grade = "A"
+    else:
+      print("You failed")
+    return grade
+print(check_grade(90))
+
+def addtwo(a,b):
+   return a+b
+print(addtwo(17,21))
+
+sum()
